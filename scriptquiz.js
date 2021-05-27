81e6777d39b94d2fbfa55449ff49dba590700a47
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2021-05-27 20:27:28 +0700

    update js  tampilan soal quiz
    menambahkan forrmat js pada tampilan soal quiz

--- scriptquiz.js
+++ scriptquiz.js
@@ -1,1 +1,37 @@
-var baru;
+var musik = new Audio();
+musik.src = "audio/bgmusik.mp3";
+musik.loop = true;
+musik.play();
+
+var filebaru;
+
+function quizscience(){
+    a = document.getElementById("Start").style.display = 'none'
+    b = document.getElementById("science").style.display = 'block'
+}
+function quizmath(){
+    a = document.getElementById("Start").style.display = 'none'
+    b = document.getElementById("mathematics").style.display = 'block'
+}
+function quizhistory(){
+    a = document.getElementById("Start").style.display = 'none'
+    b = document.getElementById("History").style.display = 'block'
+}
+
+
+
+
+//contoh format pembuatan soal
+const science = [
+    {
+        q:"hallo bang",
+        Options: ["Abdul", "Samsul", "Jamal", "Budi"],
+        answer:2  // indeks jawaban
+    },
+    {
+        q:"hallo bang",
+        Options: ["Abdul", "Samsul", "Jamal", "Budi"],
+        answer:2  // indeks jawaban
+    }
+    // lanjut
+]
