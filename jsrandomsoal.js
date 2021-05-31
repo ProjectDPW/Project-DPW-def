ca9c6dbf5aa518beb9f949978af7ef7773a098ba
200411100114_Irham Hamed Ayani <83946683+HamedAyani@users.noreply.github.com>
2021-06-01 03:15:22 +0700

    add file JavaScript
    tambah file javascript untuk RandomSoal1

---
+++ jsrandomsoal.js
@@ -0,0 +1,120 @@
+// update
+
+var questionsains = document.querySelector(".question")
+var optionCont = document.querySelector(".answer")
+var homePage = document.querySelector(".Start")
+var quizPage =document.getElementById("science")
+var resultPage = document.getElementById("skor")
+// console.log(soal1[0])
+var questcount=0;
+var currentquest;
+var availablequest=[];
+var availableoptions=[];
+var correctanswer=0;
+var attemp=0;
+
+function setavailablequest(){
+    const totalquest=soal1.length;
+    for (let i=0; i<totalquest; i++){
+        availablequest.push(soal1[i])
+        // console.log(soal1[i])
+    }
+}
+
+function getNewquest(){
+    const questionindex= availablequest[Math.floor(Math.random() * availablequest.length)]
+    currentquest=questionindex
+    // console.log(currentquest)
+
+    questionsains.innerHTML=currentquest.q;
+    const index1=availablequest.indexOf(questionindex);
+    availablequest.splice(index1,1)
+    // console.log(questionindex);
+
+    // console.log(currentquest.Options)
+    const optionlen = currentquest.Options.length
+    for (let i=0; i<optionlen; i++){
+        availableoptions.push(i)
+    }
+    optionCont.innerHTML= '';
+    // console.log(availableoptions)
+    
+    for (let i=0; i<optionlen; i++){
+        const optonIdx=availableoptions[Math.floor(Math.random()*availableoptions.length)];
+        const index2=availableoptions.indexOf(optonIdx);
+        availableoptions.splice(index2,1)
+        // console.log(optonIdx)
+
+        const option =document.createElement("div");
+        option.innerHTML=currentquest.Options[optonIdx];
+        option.id=optonIdx;
+        option.className="optional";
+        optionCont.appendChild(option);
+        option.setAttribute("onclick", "getResult(this)");
+    }
+    questcount++
+}
+
+function getResult(element){
+    const id = parseInt(element.id);
+    if (id===currentquest.answer){
+        // console.log("correct");
+        element.classList.add("correct");
+
+        correctanswer++;
+        console.log("Correct: "+correctanswer);
+    }
+    else{
+        // console.log("wrong");
+        element.classList.add("wrong")
+
+        const optionlen=optionCont.children.length;
+        for (let i=0; i<optionlen; i++){
+            if (parseInt(optionCont.children[i].id)==currentquest.answer){
+                optionCont.children[i].classList.add("correct")
+            }
+        }
+    }
+    attemp++;
+    console.log('attemp: '+attemp)
+
+    unclickOptions();
+}
+
+function unclickOptions(){
+    const optionlen=optionCont.children.length;
+    for (let i=0; i<optionlen; i++){
+        optionCont.children[i].classList.add("already-answer")
+    }
+}
+
+function nextsains(){
+    if (questcount===soal1.length){;
+        // console.log('quis over');
+        quizOver();
+    }
+    else{
+        getNewquest();
+    }
+}
+
+function quizOver(){
+    a = document.getElementById("science").style.display = 'none';
+    b = document.getElementById("skor").style.display = 'block';
+
+    quizResult();
+}
+
+function quizResult(){
+    var lensoal1=soal1.length;
+    document.getElementById("result").innerHTML=(correctanswer/lensoal1)*100;
+    document.getElementById("benar").innerHTML=correctanswer;
+    document.getElementById("salah").innerHTML=lensoal1-correctanswer;
+    // console.log("quizResult")
+}
+
+window.onload = function(){
+    setavailablequest();
+
+    getNewquest();
+}
