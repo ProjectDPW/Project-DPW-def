d2ffb3c268e508e81c5f24b3aff6123b3b27cdd9
OvadillaAisyahRahma <83963385+OvadillaAisyahRahma@users.noreply.github.com>
2021-05-31 16:53:52 -1200

    menambahkan soal1

--- scriptquiz.js
+++ scriptquiz.js
@@ -48,5 +48,141 @@ var soal1 = [
             'Air'],
         answer:3  // indeks jawaban
     },
+    {
+        q:"Gas yang paling banyak terdapat di lapisan atmosfer bumi adalah..... ",
+        Options: ['Hidrogen',
+            'Nitrogen', 
+            'Oksigen ',
+            'Karbon'],
+        answer:2  // indeks jawaban
+    },
+    {
+        q:"Dibawah ini yang bukan termasuk kategori mesin sederhana adalah..... ",
+        Options: ['Palu',
+            'Katrol', 
+            'Sekrup',
+            'Pasak'],
+        answer:1  // indeks jawaban
+    },
+    {
+        q:"Jenis batuan terkeras adalah..... ",
+        Options: ['Berlian',
+            'Tembaga', 
+            'Emas',
+            'Platinum'],
+        answer:0  // indeks jawaban
+    },
+    {
+        q:"Planet apa yang memiliki bulan paling banyak ? ",
+        Options: ['Uranus',
+            'Neptunus', 
+            'Jupiter',
+            'Saturnus'],
+        answer:3  // indeks jawaban
+    },
+    {
+        q:"Serangga bernapas dengan menggunakan apa ? ",
+        Options: ['Trakea',
+            'Paru-paru', 
+            'Kulit',
+            'Insang'],
+        answer:0  // indeks jawaban
+    },
+    {
+        q:"Berikut ini yang bukan termasuk organ pencernaan manusia adalah.....  ",
+        Options: ['Usus besar',
+            'Lambung', 
+            'Kerongkongan',
+            'Empedu'],
+        answer:3  // indeks jawaban
+    },
+    {
+        q:"Berapa jumlah perut atau lambung pada hewan sapi dan kambing ? ",
+        Options: ['Tiga',
+            'Empat', 
+            'Dua',
+            'Lima'],
+        answer:1  // indeks jawaban
+    },
+    {
+        q:"Planet yang paling dekat dengan matahari adalah..... ",
+        Options: ['Venus',
+            'Saturnus', 
+            'Merkurius',
+            'Mars'],
+        answer:2  // indeks jawaban
+    },
+    {
+        q:"Gelombang elektromagnetik yang memiliki panjang gelombang terpanjang adalah..... ",
+        Options: ['Sinar X', 
+            'Gelombang Radio', 
+            'Gelombang Mikro', 
+            'Sinar Gamma'],
+        answer:1  // indeks jawaban
+    },
+    {
+        q:"Pada usus halus terdiri atas berapa bagian ?",
+        Options: ['Tiga', 
+            'Empat',
+            'Satu', 
+            'Dua'],
+        answer:0  // indeks jawaban
+    },
+    {
+        q:"Pada bunga, bagian yang berfungsi sebagai alat kelamin betina adalah..... ",
+        Options: ['Mahkota',
+            'Benang sari',
+            'Putik',
+            'Kelopak'],
+        answer:2 // indeks jawaban
+    },
+    {
+        q:"Hewan yang makan tumbuhan, termasuk dalam kelompok..... ",
+        Options: ['Omnivora',
+            'Herbivora',
+            'Insektivora',
+            'Karnivora'],
+        answer:1 // indeks jawaban
+    },
+    {
+        q:"Hewan dibawah ini yang mengalami metamorfosis sempurna adalah..... ",
+        Options: ['Kecoa',
+            'Jangkrik', 
+            'Kupu-kupu', 
+            'Semut'],
+        answer:2 // indeks jawaban
+    },
+    {
+        q:"Lingkungan yang berupa benda-benda mati dinamakan lingkungan..... ",
+        Options: ['Biotik', 
+            'Biologi', 
+            'Abiotik',
+            'Antik'],
+        answer:2 // indeks jawaban
+    },
+    {
+        q:"Bagian yang mempunyai klorofil adalah..... ",
+        Options: ['Batang',
+            'Daun', 
+            'Akar',
+            'Bunga '],
+        answer:1 // indeks jawaban
+    },
+    {
+        q:"Selain sebagai Indra pembau, hidung berfungsi sebagai..... ",
+        Options: ['Saluran pernapasan',
+            'Saluran pencernaan',
+            'Alat keseimbangan',
+            'Saluran pendengaran'],
+        answer:0 // indeks jawaban
+    },
+    {
+        q:"Setelah dewasa manusia berubah menjadi? ",
+        Options: ['Remaja', 
+            'Anak-anak',
+            'Tua', 
+            'Bayi'],
+        answer:2 // indeks jawaban
+    },
 ]
 
